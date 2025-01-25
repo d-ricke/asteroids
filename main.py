504bae4e5cf5f4016f2d463b494f8f4d2d241495
d-ricke <15657574+d-ricke@users.noreply.github.com>
2025-01-24 23:57:24 -0700

    built a player ship

--- main.py
+++ main.py
@@ -2,16 +2,15 @@
 # the open-source pygame library
 # throughout this file
 import pygame
+import player
 from constants import SCREEN_WIDTH, SCREEN_HEIGHT, ASTEROID_MIN_RADIUS, ASTEROID_KINDS, ASTEROID_SPAWN_RATE, ASTEROID_MAX_RADIUS
 
 def main():
     pygame.init()
-    #rint("Starting asteroids!")
-    #print(f"Screen width: {SCREEN_WIDTH}\nScreen height: {SCREEN_HEIGHT}")
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-    
     clock = pygame.time.Clock()
     dt = 0
+    player_ship = player.Player(x=SCREEN_WIDTH/2, y=SCREEN_HEIGHT/2)
 
     while (True):
         for event in pygame.event.get():
@@ -19,7 +18,10 @@ def main():
                 return
             
         screen.fill("Black")
+        player.draw(screen)
         pygame.display.flip()
+
+        # limit the framerate to 60 FPS
         dt = clock.tick(60) / 1000
 
 if __name__ == "__main__":
